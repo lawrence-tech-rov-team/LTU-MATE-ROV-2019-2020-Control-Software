1201df85e48642d92ddf126a734b79fcd411a62e
zcareyltu <zcarey@ltu.edu>
2019-10-01 14:05:01 -0400

    Add project files.

---
+++ LTU MATE ROV 2019-2020 Control Software/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LTU_MATE_ROV_2019_2020_Control_Software {
+	static class Program {
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main() {
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+		}
+	}
+}
