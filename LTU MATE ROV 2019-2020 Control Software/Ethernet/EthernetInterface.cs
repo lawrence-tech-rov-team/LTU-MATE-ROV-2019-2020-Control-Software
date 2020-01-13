b5f2030a0ba1eec0d07385a110dfe2b4c403c522
zcareyltu <zcarey@ltu.edu>
2020-01-13 15:31:57 -0500

    Added ethernet interface

---
+++ LTU MATE ROV 2019-2020 Control Software/Ethernet/EthernetInterface.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTU_MATE_ROV_2019_2020_Control_Software.Ethernet {
+	public class EthernetInterface {
+
+		public int DestinationPort { get; } = 6001;
+		public int ReceivePort { get; } = 6002;
+		public string TargetIp { get; set; } = "169.254.240.157"; //255.255.255.255
+		public bool Connected { get; private set; } = false;
+
+		private UdpClient client;
+		private Random random = new Random();
+
+		public delegate void NewPacketHandler(UdpPacket packet);
+		public event NewPacketHandler OnPacketReceived;
+
+		public EthernetInterface() {
+
+		}
+
+		~EthernetInterface() {
+			Disconnect();
+		}
+
+		private bool AutoConnect() {
+			int origSendTimeout = 5000;
+			int origRecvTimeout = 5000;
+
+			try {
+				origSendTimeout = client.Client.SendTimeout;
+				origRecvTimeout = client.Client.ReceiveTimeout;
+
+				client.Client.SendTimeout = 100;
+				client.Client.ReceiveTimeout = 100;
+			} catch (Exception ex) { }
+
+			IPEndPoint ip = new IPEndPoint(IPAddress.Any, ReceivePort);
+			byte[] pings = new byte[10];
+			random.NextBytes(pings);
+			int counts = 0;
+			for (int i = 0; i < 10; i++) {
+				try {
+					Console.Out.WriteLine("Attempt #{0}...", i);
+					UdpPacket packet = new UdpPacket(Command.Ping, pings[i]);
+					byte[] data = packet.AllBytes;
+					client.Send(data, data.Length, TargetIp, DestinationPort);
+					data = client.Receive(ref ip);
+					packet = UdpPacket.ParseData(data);
+					if ((packet != null) && (packet.Command == Command.Ping) && (packet.Data.Length == 1) && (packet.Data[0] == pings[i])) {
+						counts++;
+					}
+				} catch (Exception ex) {
+					Console.Error.WriteLine("Error sending/receiving packet: {0}", ex.Message);
+					Console.Error.WriteLine(ex.StackTrace);
+				}
+			}
+
+			try {
+				client.Client.SendTimeout = origSendTimeout;
+				client.Client.ReceiveTimeout = origRecvTimeout;
+			} catch (Exception ex) { }
+
+			Console.WriteLine("Counts: {0}", counts);
+
+			return counts >= 5; //TODO try up to ten times, but if one suceeds then it will try five times, not counting towards the ten
+		}
+
+		public bool TryConnect() {
+			Disconnect();
+
+			try {
+				client = new UdpClient();
+				client.Client.Bind(new IPEndPoint(IPAddress.Any, ReceivePort));
+				bool success = AutoConnect();
+				//client.Connect(new IPEndPoint(IPAddress.Any, ReceivePort));
+				/*int origSend = client.Client.SendTimeout;
+				int origRecv = client.Client.ReceiveTimeout;
+				client.Client.SendTimeout = client.Client.ReceiveTimeout = 1000;
+				bool success = false;
+				try {
+					IPEndPoint ip = new IPEndPoint(IPAddress.Any, ReceivePort);
+					byte[] pings = new byte[10];
+					random.NextBytes(pings);
+					int counts = 0;
+					for (int i = 0; i < 10; i++) {
+						Console.Out.WriteLine("Attempt #{0}...", i);
+						UdpPacket packet = new UdpPacket(Command.Ping, pings[i]);
+						byte[] data = packet.AllBytes;
+						client.Send(data, data.Length, TargetIp, DestinationPort);
+						try {
+							data = client.Receive(ref ip);
+						} catch (SocketException) { }
+						packet = UdpPacket.ParseData(data);
+						if((packet != null) && (packet.Command == Command.Ping) && (packet.Data != null) && (packet.Data.Length >= 1) && (packet.Data[0] == pings[0])) {
+							counts++;
+							if (counts >= 3) break;
+						}
+					}
+					if (counts >= 3) success = true;
+				} catch (Exception ex2) {
+					Console.Error.WriteLine("Error trying to connect: {0}", ex2.Message);
+					Console.Error.WriteLine(ex2.StackTrace);
+					success = false;
+				}
+
+				client.Client.SendTimeout = origSend;
+				client.Client.ReceiveTimeout = origRecv;*/
+				if (success) client.BeginReceive(new AsyncCallback(OnDataReceived), null);
+				return success;
+			} catch (Exception ex) {
+				try {
+					client.Close();
+				} catch (SocketException) {
+				}
+
+				Console.Error.WriteLine("Could not connect to IP: {0}", ex.Message);
+				Console.Error.WriteLine(ex.StackTrace);
+				return false;
+			}
+		}
+
+		public void Disconnect() {
+			try {
+				if (client != null) client.Close();
+			} catch (SocketException e) {
+				//TODO exception thrown
+			}
+			//TODO callback on close.
+		}
+
+		public bool Send(UdpPacket packet) {
+			try {
+				byte[] data = packet.AllBytes;
+				client.Send(data, data.Length, TargetIp, DestinationPort);
+				return true;
+			} catch (Exception ex) {
+				Console.Error.WriteLine("Error sending packet: {0}", ex.Message);
+				Console.Error.WriteLine(ex.StackTrace);
+				return false;
+			}
+		}
+
+		public bool Send(Command command, params byte[] data) {
+			return Send(new UdpPacket(command, data));
+		}
+
+		public bool Send(Command command, string message, bool clip = true) {
+			if (clip || (message.Length <= UdpPacket.MAX_LENGTH))
+				return Send(new UdpPacket(command, Encoding.UTF8.GetBytes(message)));
+			else {
+				bool result = Send(new UdpPacket(command, Encoding.UTF8.GetBytes(message.Substring(0, UdpPacket.MAX_LENGTH))));
+				if (!result) return false;
+				return Send(command, message.Substring(UdpPacket.MAX_LENGTH), clip);
+			}
+		}
+
+		private void OnDataReceived(IAsyncResult res) {
+			try {
+				IPEndPoint ip = new IPEndPoint(IPAddress.Any, ReceivePort);
+				byte[] data = client.EndReceive(res, ref ip);
+				UdpPacket packet = UdpPacket.ParseData(data);
+				if (packet != null) OnPacketReceived?.Invoke(packet); //TODO don't catch exception for called function.
+				else Console.Out.WriteLine("Bad packet received.");
+				client.BeginReceive(new AsyncCallback(OnDataReceived), null);
+			} catch (Exception e) {
+				Disconnect();
+			}
+		}
+	}
+}
---
+++ LTU MATE ROV 2019-2020 Control Software/Ethernet/UdpPacket.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTU_MATE_ROV_2019_2020_Control_Software.Ethernet {
+	public class UdpPacket {
+		public static readonly byte START_BYTE = 0xFF;
+		public static readonly byte CHECKSUM_MASK = 0xFF;
+		public static readonly int MAX_LENGTH = 255;
+
+		//public byte StartByte;
+		public Command Command;
+		//public byte Length;
+		public byte[] Data;
+		public byte Checksum;
+
+		public byte[] AllBytes {
+			get {
+				byte[] bytes = new byte[Data.Length + 3];
+				bytes[0] = START_BYTE;
+				bytes[1] = (byte)Command;
+				Array.Copy(Data, 0, bytes, 2, Data.Length);
+				bytes[bytes.Length - 1] = Checksum;
+				return bytes;
+			}
+		}
+
+		private UdpPacket() {
+		}
+
+		public UdpPacket(Command command, params byte[] data) {
+			this.Command = command;
+			if (data == null) this.Data = new byte[0];
+			else if (data.Length <= MAX_LENGTH) this.Data = data;
+			else {
+				this.Data = new byte[MAX_LENGTH];
+				Array.Copy(data, this.Data, MAX_LENGTH);
+			}
+			Checksum = (byte)(START_BYTE + command);
+			foreach (byte b in Data) Checksum += b;
+			Checksum &= CHECKSUM_MASK;
+		}
+
+		public static UdpPacket ParseData(byte[] data) {
+			if (data.Length < 3) return null;
+
+			UdpPacket packet = new UdpPacket();
+			//packet.StartByte = data[0];
+			packet.Command = (Command)data[1];
+			//packet.Length = data[2];
+			packet.Checksum = data[data.Length - 1];
+
+			if ((data[0] != START_BYTE)
+				//|| (packet.Command == START_BYTE)
+				//|| (packet.Length == START_BYTE)
+				/*|| (packet.Checksum == START_BYTE)*/) return null;
+
+			//Check both for invalid bytes and the checksum.
+			byte checksum = 0;
+			for (int i = 0; i < (data.Length - 1); i++) {
+				//				if (data[i] == START_BYTE) return null;
+				checksum += data[i];
+			}
+
+			if ((checksum & CHECKSUM_MASK) != data[data.Length - 1]) return null;
+
+			packet.Data = new byte[data.Length - 3];
+			Array.Copy(data, 2, packet.Data, 0, data.Length - 3);
+
+			return packet;
+		}
+
+	}
+}
