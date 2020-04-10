4881840b0485b7af363fa8880206360e6a8503e3
zcareyltu <zcarey@ltu.edu>
2020-04-09 22:30:20 -0400

    Generic Joystick config

---
+++ LTU MATE ROV 2019-2020 Control Software/InputControls/Joysticks/JoystickConfig.cs
@@ -0,0 +1,50 @@
+﻿using JoystickInput;
+using LTU_MATE_ROV_2019_2020_Control_Software.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTU_MATE_ROV_2019_2020_Control_Software.InputControls.Joysticks {
+	public abstract class JoystickConfig {
+
+		public abstract Twist Translate(Dictionary<JoystickControl, int> states);
+
+		public static JoystickConfig GetConfig(string name) {
+			switch (name) {
+				case "Thrustmaster T.16000M":
+				case "Thrustmaster T.16000M Throttle":
+				case "Thrustmaster T.Flight Rudder Car Mode":
+				case "Thrustmaster T.Flight Rudder":
+				case "Thrustmaster T.Flight HOTAS X":
+				case "Thrustmaster Warthog Joystick":
+				case "Thrustmaster Warthog Throttle":
+				case "Thrustmaster Warthog Combined":
+				case "Saitek Rudder Pedals":
+				case "Saitek Combat Rudder Pedals":
+				case "CH Pro Pedals":
+				case "VKB Gunfighter":
+				case "MFG Crosswind":
+				case "Saitek X52 HOTAS":
+				case "Saitek X52 Pro HOTAS":
+				case "Saitek X55 Throttle":
+				case "Saitek X55 Joystick":
+				case "Saitek X56 Throttle":
+				case "Saitek X56 Joystick":
+				case "Saitek Pro Flight Throttle Quadrant":
+				case "Saitek X45 HOTAS (2541)":
+				case "Saitek X45 HOTAS (053c)":
+				case "Saitek Cyborg Evo":
+				case "Virpil Mongoos T-50":
+				case "VPC-Star-Citizen-L":
+				case "VPC-Star-Citizen-R":
+				case "Logitech 3D Pro":
+				default:
+					return new Logitech3dProConfig();
+			}
+		}
+
+	}
+
+}
---
+++ LTU MATE ROV 2019-2020 Control Software/InputControls/Joysticks/Logitech3dProConfig.cs
@@ -0,0 +1,36 @@
+﻿using JoystickInput;
+using LTU_MATE_ROV_2019_2020_Control_Software.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTU_MATE_ROV_2019_2020_Control_Software.InputControls.Joysticks {
+	public class Logitech3dProConfig : JoystickConfig {
+
+		public override Twist Translate(Dictionary<JoystickControl, int> states) {
+			Twist twist = new Twist();
+
+			float speed = 1f - states[JoystickControl.Sliders0] / UInt16.MaxValue;
+
+			float hat = getAngle(states[JoystickControl.PointOfViewControllers0]);
+			twist.Linear.X = (hat < 0) ? 0f : (float)Math.Sin(hat);
+			twist.Linear.Y = (hat < 0) ? 0f : (float)Math.Cos(hat);
+			twist.Linear.Z = ((states[JoystickControl.Buttons3] > 0) ? 1f : 0f) + ((states[JoystickControl.Buttons2] > 0) ? -1f : 0f);
+
+			twist.Angular.X = (float)states[JoystickControl.X] / ushort.MaxValue * 2f - 1f;
+			twist.Angular.Y = (float)states[JoystickControl.Y] / ushort.MaxValue * 2f - 1f;
+			twist.Angular.Z = (float)states[JoystickControl.Z] / ushort.MaxValue * 2f - 1f;
+
+			return twist;
+		}
+
+		private float getAngle(int id) {
+			if (id < 0) return -1;
+			float angle = id / 100;
+			return (float)(angle * Math.PI / 180);
+		}
+
+	}
+}
