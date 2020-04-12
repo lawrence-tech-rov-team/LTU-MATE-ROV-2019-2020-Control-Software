b0e6cf82373194111180952fe03ee8fda8fdfdde
Zachary Carey <zcarey@ltu.edu>
2020-04-12 13:59:42 -0400

    Controls thread base

--- LTU MATE ROV 2019-2020 Control Software/Controls/ControlsThread.cs
+++ LTU MATE ROV 2019-2020 Control Software/Controls/ControlsThread.cs
@@ -1,13 +1,37 @@
-﻿using System;
+﻿using LTU_MATE_ROV_2019_2020_Control_Software.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace LTU_MATE_ROV_2019_2020_Control_Software.Controls {
 	public class ControlsThread {
 
+		private Thread thread;
+		private ThreadPriority priority;
+		private volatile bool running = true;
 
+		public ControlsThread(ThreadPriority priority) {
+			this.priority = priority;
+			thread = ThreadHelper.StartNewThread("Controls Thread", true, ThreadLoop, priority);
+		}
+
+		public void StopAsync() {
+			running = false;
+		}
+
+		public void Stop() {
+			StopAsync();
+			thread.Join();
+		}
+
+		private void ThreadLoop() {
+			while (running) {
+
+			}
+		}
 
 	}
 }
--- LTU MATE ROV 2019-2020 Control Software/ILogging.cs
+++ LTU MATE ROV 2019-2020 Control Software/ILogging.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace LTU_MATE_ROV_2019_2020_Control_Software {
-	public interface ILogging {
+	public interface ILogging { //TODO logging isn't being utilized
 	}
 
 	public static class ILoggingExtensions {
