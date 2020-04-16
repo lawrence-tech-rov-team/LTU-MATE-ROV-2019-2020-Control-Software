022885f3e499c4b30a680df7494ae965a9dff1e2
Zachary Carey <zcarey@ltu.edu>
2020-04-16 16:34:39 -0400

    Started robot thread

--- LTU MATE ROV 2019-2020 Control Software/Robot/RobotThread.cs
+++ LTU MATE ROV 2019-2020 Control Software/Robot/RobotThread.cs
@@ -8,112 +8,23 @@ using System.Threading;
 using System.Threading.Tasks;
 
 namespace LTU_MATE_ROV_2019_2020_Control_Software.Robot {
-	public static class RobotThread {
+	public class RobotThread : ThreadedProcess {
 
-		private static Thread thread;
-
-		//private static IController controller = null;
-		private static volatile bool runThread = false;
-
-		private static volatile bool ShouldSwitchControls = false;
-		private static readonly object ControlLock = new object();
-
-		private static volatile bool RequestInputData = false;
-		private static volatile InputControlData RequestedInputData = null;
-		//private static volatile IKeyboardListener keyboardListener = null;
-		private static readonly object InputDataLock = new object();
-		
-
-		static RobotThread() {
-
+		public RobotThread(ThreadPriority Priority = ThreadPriority.Normal) : base("Robot Thread", Priority) {
 		}
 
-		public static bool Start() {
-			if (thread != null) return true;
-			runThread = true;
-			//thread = ThreadHelper.StartNewThread("Robot", false, Loop);
-			return thread != null;
+		protected override void Initialize() {
+			throw new NotImplementedException();
 		}
 
-		public static void RequestStop() {
-			//AKA set a flag to stop the thread without waiting for it end.
-			runThread = false;
+		protected override bool Loop() {
+			throw new NotImplementedException();
 		}
 
-		public static void Stop() {
-			runThread = false;
-			thread.Join();
-		}
-/*
-		public static void SetControllerType(ControllerType type, IKeyboardListener keyboard = null) {
-			lock (ControlLock) {
-				SwitchControlType = type;
-				keyboardListener = keyboard;
-				ShouldSwitchControls = true;
-			}
-		}
-		*/
-		public static InputControlData GetInputData() {
-			lock (InputDataLock) {
-				RequestInputData = true;
-				while (RequestInputData == true) ;
-				return RequestedInputData;
-			}
-		}
-
-		private static void Loop() {
-			while (runThread) {
-				/*if (controller != null) {
-					controller.Update();
-				}
-
-				if (RequestInputData) {
-					if (controller == null) {
-						RequestedInputData = null;
-					} else {
-						RequestedInputData = new InputControlData(controller.ForwardPower, controller.SidePower);
-					}
-					RequestInputData = false;
-				}*/
-/*
-				ControllerType? swap = null;
-				IKeyboardListener keyboard = null;
-				lock (ControlLock) {
-					if (ShouldSwitchControls) {
-						swap = SwitchControlType;
-						keyboard = keyboardListener;
-						ShouldSwitchControls = false;
-					}
-				}
-
-				if(swap != null) {
-					if (controller != null) controller.Disconnect();
-					controller = ((ControllerType)swap).GetNewController();
-					if (!controller.Connect()) { //TODO crashes if doesn't exist
-						Console.WriteLine("Failed to connect to control device!");
-						controller = null;
-					}else if(controller is KeyboardController) {
-						((KeyboardController)controller).KeyListener = keyboard;
-					}
-				}*/
-
-				Thread.Sleep(34); //TODO temporary for proof of concept
-			}
+		protected override void Cleanup() {
+			throw new NotImplementedException();
 		}
 
 	}
 
-	public class InputControlData {
-		public float ForwardThrust;
-		public float SideThrust;
-
-		public InputControlData() {
-
-		}
-		
-		public InputControlData(float f, float s) {
-			ForwardThrust = f;
-			SideThrust = s;
-		}
-	}
 }
